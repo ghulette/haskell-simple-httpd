b5c403d3e5cc139ad0dcbe3ec8af075ff4a2ae5b
Geoff Hulette <ghulette@gmail.com>
2010-09-28 16:38:15 -0700

    Fixed bug in delivering binary files

--- Httpd.hs
+++ Httpd.hs
@@ -4,10 +4,11 @@
 module Httpd (runServer) where
 import Control.Concurrent (forkIO)
 import Control.Exception (try,IOException)
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
 import Network
 import Network.URI
 import System.IO
-
 
 -- Server stuff
 
@@ -30,15 +31,15 @@ handleRequest root h host port = do
   case parseRequest reqStr of
     Just req -> do
       let file = root ++ (unEscapeString . show . requestURI $ req)
-      result <- try $ readFile file
-      case result :: Either IOException String of
+      result <- try $ B.readFile file
+      case result :: Either IOException ByteString of
         Left err -> do
           putStrLn $ "File not found " ++ file
           hPutStrLn h "HTTP/1.1 404 NOT FOUND\n"
         Right body -> do
           putStrLn $ "Serving " ++ file
           hPutStrLn h "HTTP/1.1 200 OK\n"
-          hPutStrLn h body
+          B.hPutStr h body
     Nothing -> do
       putStrLn "Could not parse request:"
       putStrLn reqStr
